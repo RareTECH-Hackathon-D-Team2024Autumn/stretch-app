4dc84b74a5c1cb7cb2fbcc4d844bffe5153be795
megane575 <rie-fttm0129@gmail.com>
2024-10-30 13:57:53 +0900

    ログイン画面のhtmlとcssを作成

--- front/app/src/App.js
+++ front/app/src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import {useState} from "react";
 import './App.css';
 
 function App() {
+  const initialValues = {mailAddress: "",password: ""};
+  const [formValues,setForValues] = useState(initialValues);
+
+  const handleChange = (e) => {
+    // 
+    const { name, value } =e.target;
+    setForValues({...formValues,name:value});
+
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="formContainer">
+      <form>
+        <h1>ほぐし〜の</h1>
+        <div className="uiForm">
+          <div className="formField">
+            <lavel>メールアドレス</lavel>
+            <input 
+              type="text" 
+              placeholder="メールアドレスを入力してください" 
+              name="mailAddress" 
+              onChange={(e) => handleChange(e)}/>
+          </div>
+          <div className="formField">
+            <lavel>パスワード</lavel>
+            <input type="text" placeholder="パスワードを入力してください" name="password"/>
+          </div>
+          <div className= "Button">
+            <button className="submitButton">新規登録</button>
+            <button className="loginButton">ログイン</button>
+          </div>
+        </div>
+      </form>
+      
     </div>
   );
 }
