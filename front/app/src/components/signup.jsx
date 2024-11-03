e7c9e85381709db1753c1e7b87cbc996fcebc4b4
mo-ri-regen <moricontb@gmail.com>
2024-11-03 17:01:05 +0900

    💄　新規登録画面　HTML部分を追加

---
+++ front/app/src/components/signup.jsx
@@ -0,0 +1,40 @@
+
+
+// import '../signup.css';
+
+export default function signup(){
+    return(
+      <>
+      <div className="formContainer">
+      
+        <form action='submit' methods=''>
+            
+            <h1>ほぐし〜の</h1>
+            <hr></hr>            
+            <div className='uiForm'>
+
+                <div className="formField">
+                    <label for='name'>
+                        ユーザー名
+                        <input type='text' placeholder="ユーザー名を入力" name="username"></input>
+                    </label>
+                </div>
+                <div className="formField">
+                    <label for='name'>
+                        メールアドレス
+                        <input type='text' placeholder="メールアドレスを入力" name="email"></input>
+                    </label>
+                </div>
+                <div className="formField">
+                    <label for='name'>
+                        パスワード
+                        <input type='text' placeholder="パスワードを入力" name="password"></input>
+                    </label>
+                </div>
+                <button className='submitButton'>登録</button>                                    
+            </div>
+        </form>
+      </div>
+      </>
+    );
+  }
