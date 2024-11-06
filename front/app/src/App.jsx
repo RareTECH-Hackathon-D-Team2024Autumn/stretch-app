2579db4f22e12ccb15e1c49348d674409f793adf
mo-ri-regen <moricontb@gmail.com>
2024-11-07 01:19:58 +0900

    ルーティング機能を追加

--- front/app/src/App.jsx
+++ front/app/src/App.jsx
@@ -1,7 +1,18 @@
 import {useState} from "react";
 import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 
 function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
