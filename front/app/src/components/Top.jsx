abc768a2f019c4036fc33725f3b64ea72eed72da
megane575 <rie-fttm0129@gmail.com>
2024-11-10 02:36:19 +0900

    Topページのhtml、cssを作成

--- front/app/src/components/Top.jsx
+++ front/app/src/components/Top.jsx
@@ -1,7 +1,46 @@
+import React from 'react';
+import "../styles/Top.css";
+
 function Top() {
   return (
     <>
-      <h1>Hello</h1>;
+      <header className="header">
+        <div className="top-page">Topページ</div>
+        <div>
+          <button className="align-button">ユーザー情報</button>
+        </div>
+      </header>
+      <main className="maincontainer">
+        <div>
+          <div className="favoritecontainer">
+            <p className="favoritecheck">お気に入り</p>
+          </div>
+          <div className="urlcontainer">
+            <a href="https://www.youtube.com/">ここに動画のURL情報が載る</a>
+          </div>
+          <div className="urlcontainer">
+            <a href="https://www.youtube.com/">ここに動画のURL情報が載る</a>
+          </div>
+          <div className="urlcontainer">
+            <a href="https://www.youtube.com/">ここに動画のURL情報が載る</a>
+          </div>
+          <div className="urlcontainer">
+            <a href="https://www.youtube.com/">ここに動画のURL情報が載る</a>
+          </div>
+          <div className="urlcontainer">
+            <a href="https://www.youtube.com/">ここに動画のURL情報が載る</a>
+          </div>
+          <div className="urlcontainer">
+            <a href="https://www.youtube.com/">ここに動画のURL情報が載る</a>
+          </div>
+          <div className="urlcontainer">
+            <a href="https://www.youtube.com/">ここに動画のURL情報が載る</a>
+          </div>
+        </div>
+      </main>
+      <footer className="footer">
+        <button className="footer-button">お気に入り一覧へ</button>
+      </footer>
     </>
   );
 }
