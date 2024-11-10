81e8053b682a0c77cc6563662834153d194b7630
KikuS <KikuS0211@outlook.jp>
2024-11-11 01:43:59 +0900

    環境変数のコメントを ソルト値->ペッパー値へ変更 top_pages, youtube_videoes テーブルの定義

--- back/app/models.py
+++ back/app/models.py
@@ -15,9 +15,15 @@ from flask_login import UserMixin
 # データベース関数を使用数際に利用するfunc（名前付きのSQL関数を使用する）をインポート
 from sqlalchemy.sql import func
 
+# 外部キーをしようするためのモジュールのインポート
+from sqlalchemy import ForeignKey
+
+# 外部キーとのリレーションを行うためのモジュールのインポート
+from sqlalchemy.orm import relationship
+
 # .envファイルを呼び出す
 load_dotenv('/back/.env')
-# .envファイル内のソルト値を変数に代入
+# .envファイル内のペッパー値を変数に代入
 PEPPER = os.getenv('PEPPER')
 
 ## load_user関数
@@ -29,27 +35,24 @@ def load_user(user_id):
 ## Userテーブルの定義
 class User(UserMixin, db.Model):
     # テーブル名
-    __tablename__ = 'Users'
+    __tablename__ = 'users'
 
     # カラム定義
-    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True, nullable=False,)
     # user_nameとmail_addressは検索を早くするためにindex=Trueの追加
     user_name = db.Column(db.String(30), index=True, nullable=False)
     mail_address = db.Column(db.String(256), unique=True, index=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
-    # DateTimeのデフォルト値をユーザー作成時のサーバーの時刻とする
-    created_at = db.Column(db.DateTime, server_default=func.now())
-     # DateTimeのデフォルト値をユーザー作成時のサーバーの時刻とする
-    updated_at = db.Column(db.DateTime, server_default=func.now())
+    users = relationship('Top', backref='User')
 
     # Userクラスをインスタンス化して各カラムを引数で使用できるようにしている
     def __init__(self, mail_address, user_name, password):
         self.mail_address = mail_address
         self.user_name = user_name
-        # 入力したパスワードとソルト値を組み合わせたものをハッシュ化する
+        # 入力したパスワードとペッパー値を組み合わせたものをハッシュ化する
         self.password = generate_password_hash(password + PEPPER)
 
-    # パスワードにソルト値を足してハッシュ化したものと、パスワードにソルト値を足したものを比べ判定している
+    # パスワードにペッパー値を足してハッシュ化したものと、パスワードにペッパー値を足したものを比べ判定している
     def validate_password(self, password):
         password = password + PEPPER
         return check_password_hash(self.password, password)
@@ -59,5 +62,35 @@ class User(UserMixin, db.Model):
     def select_by_mail_address(cls, mail_address):
         return cls.query.filter_by(mail_address=mail_address).first()
 
+# youtube_videosテーブルの作成
+class YouTube(UserMixin, db.Model):
+    # テーブル名
+    __tablename__ = 'youtube_videoes'
+
+    # カラム定義
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    # user_nameとmail_addressは検索を早くするためにindex=Trueの追加
+    item = db.Column(db.String(1000), nullable=False)
+    url = db.Column(db.String(100), nullable=False)
+    thumbnail = db.Column(db.String(100), nullable=False)
+    # DateTimeのデフォルト値をユーザー作成時のサーバーの時刻とする
+    created_at = db.Column(db.DateTime, server_default=func.now())
+     # DateTimeのデフォルト値をユーザー作成時のサーバーの時刻とする
+    updated_at = db.Column(db.DateTime, server_default=func.now())
+    videoes = relationship('Top', backref='YouTube')
+
+
+#top_pagesテーブルの作成
+class Top(UserMixin, db.Model):
+    # テーブル名
+    __tablename__ = 'top_pages'
+
+    # カラム定義
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    user_id = db.Column(db.Integer, ForeignKey('users.id') ,nullable=False)
+    video_id = db.Column(db.Integer, ForeignKey('youtube_videoes.id') ,nullable=False)
+    my_favorite = db.Column(db.Boolean, default=False, nullable=False)
+    # DateTimeのデフォルト値をユーザー作成時のサーバーの時刻とする
+    created_at = db.Column(db.DateTime, server_default=func.now())
 
 
