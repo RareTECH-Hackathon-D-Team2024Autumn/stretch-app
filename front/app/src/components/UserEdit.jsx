efeed333fd0780b19aeb0bd45eaeba02b7f69084
mo-ri-regen <moricontb@gmail.com>
2024-11-15 00:12:15 +0900

    #43 html作成

--- front/app/src/components/UserEdit.jsx
+++ front/app/src/components/UserEdit.jsx
@@ -3,7 +3,34 @@ export default function UserEdit() {
   return (
     <>
       <Header />
-      <h1>hello</h1>
+      <div>編集したい項目の編集ボタンを押して保存で編集ができます</div>
+      <div>
+        <input
+          name="username"
+          type="text"
+          placeholder="ユーザー名"
+          disabled
+        ></input>
+        <button>編集</button>
+      </div>
+      <div>
+        <input
+          name="email"
+          type="text"
+          placeholder="メールアドレス"
+          disabled
+        ></input>
+        <button>編集</button>
+      </div>
+      <div>
+        <input
+          name="パスワード"
+          type="password"
+          placeholder="パスワード"
+          disabled
+        ></input>
+        <button>編集</button>
+      </div>
     </>
   );
 }
