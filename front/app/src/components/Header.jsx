fc2c351dd5f43ec2d315a4d5356dab558050caa2
mo-ri-regen <moricontb@gmail.com>
2024-11-23 12:23:20 +0900

    #43 pathによって名称変更するように修正

--- front/app/src/components/Header.jsx
+++ front/app/src/components/Header.jsx
@@ -1,8 +1,14 @@
+import { useLocation } from "react-router-dom";
+
 export default function Header() {
+  const pathName = useLocation().pathname;
   return (
     <>
       <header className="header">
-        <div className="top-page">Topページ</div>
+        {pathName === "/top" && <div className="top-page">Topページ</div>}
+        {pathName === "/UserEdit" && (
+          <div className="top-page">ユーザー情報編集</div>
+        )}
       </header>
     </>
   );
