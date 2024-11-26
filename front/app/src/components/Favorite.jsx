f763993259931b65b5637a3c6ae3af86cd6896c6
mo-ri-regen <moricontb@gmail.com>
2024-11-26 23:19:10 +0900

    #76 遷移ボタン追加

--- front/app/src/components/Favorite.jsx
+++ front/app/src/components/Favorite.jsx
@@ -1,9 +1,17 @@
+import { useNavigate } from "react-router-dom";
 import Header from "./Header";
 
 export default function Favorite() {
+  const navigate = useNavigate();
+  const handleTopButton = () => {
+    navigate("/top");
+  };
   return (
     <>
       <Header />
+      <button className="top-button" onClick={handleTopButton}>
+        トップページへ
+      </button>
     </>
   );
 }
